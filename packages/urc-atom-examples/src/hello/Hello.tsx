6934731b71b2686ea0417d722baef4d2633cc7f8
Stefan Zerkalica <zerkalica@gmail.com>
2018-11-26 23:17:54 +0300

    fix(urc-atom-examples): new actions

--- packages/urc-atom-examples/src/hello/Hello.tsx
+++ packages/urc-atom-examples/src/hello/Hello.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import {observer, Deps} from '../common'
-import {mem, action, action_sync, fail} from 'urc-atom'
+import {mem, action, fail} from 'urc-atom'
 
 class HelloModel {
     protected fetch: <V>(url: string, init?: RequestInit) => V
@@ -49,8 +49,8 @@ class HelloModel {
         this.userChanged = undefined
     }
 
-    @action_sync
-    setUser(e: React.ChangeEvent<any>) {
+    @action.sync
+    setUser(e: React.ChangeEvent<HTMLInputElement>) {
         this.user = e.target.value
     }
 }
